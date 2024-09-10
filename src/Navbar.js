c706493af3051f8ae36844c453f32f86f1ff8b97
maaz-kang-92 <127050644+maaz-kang-92@users.noreply.github.com>
2024-09-11 03:40:44 +0500

    dd

--- src/Navbar.js
+++ src/Navbar.js
@@ -13,13 +13,13 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/textform">Home</a>
+                <a className="nav-link active" aria-current="page" href="#textform">Home</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">Link</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">About</a>
+                <a className="nav-link" href="#about">About</a>
               </li>
             </ul>
 
